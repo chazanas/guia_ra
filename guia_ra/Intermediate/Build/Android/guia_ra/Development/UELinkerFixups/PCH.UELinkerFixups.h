14212ed00e9d76ed7483a28c1f922ad3aa10a5a1
chazanas <chazanaspln@gmail.com>
2018-11-04 18:15:30 -0200

    new project

---
+++ guia_ra/Intermediate/Build/Android/guia_ra/Development/UELinkerFixups/PCH.UELinkerFixups.h
@@ -0,0 +1,2 @@
+// PCH for C:\guia_ra\guia_ra\Intermediate\Build\Android\UE4\Development\UELinkerFixupsName.h
+#include "C:\guia_ra\guia_ra\Intermediate\Build\Android\UE4\Development\UELinkerFixupsName.h"
